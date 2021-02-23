902ec9ab4ee79fa617d73a34c5f8753a1c16fca3
Paul <sevath1996@gmail.com>
2021-02-23 11:54:58 -0600

    Addition of jumping
    Added jumping with animation. 
    Known but : Animation when moving  forward.

--- Assets/Scripts/PlayerBasicMovement.cs
+++ Assets/Scripts/PlayerBasicMovement.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class PlayerBasicMovement : MonoBehaviour
 {
+    //Moving variables
     public float movementSpeed;
+    public float jumpHeight;
+    //Jumping variables
+    private bool canMove;
+    private bool isGrounded;
+    //Compoonent variables
     private Rigidbody rb;
     private Animator animate;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Grabbing component variales
         rb = GetComponent<Rigidbody>();
         animate = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        //Start movement function.
         PlayerMovement(movementSpeed);
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W))
+        //When the player wants to jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpHeight);
+            isGrounded = false;
+            animate.SetBool("Jump", true);
+        }
+        //Animation for when the player is movinng.
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) && canMove)
         {
             animate.SetBool("Walking", true);
         }
@@ -37,5 +53,13 @@ public class PlayerBasicMovement : MonoBehaviour
         Vector3 playerMovement = new Vector3(hor, 0f, ver) * moveSpeed * Time.deltaTime;
         //This setting moves our player based on the value of playerMovement, using the RIGIDBODY attached to the player.
         transform.Translate(playerMovement, Space.Self);
+    }
+    void OnCollisionEnter(Collision col) 
+    {
+        if (col.gameObject.tag == "Ground") 
+        {
+            isGrounded = true;
+            animate.SetBool("Jump", false);
+        }
     }
 }
