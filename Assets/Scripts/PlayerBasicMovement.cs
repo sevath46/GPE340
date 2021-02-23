9d5e5d02dbad010511b9573125bf87409e3c058e
Paul <sevath1996@gmail.com>
2021-02-23 11:20:05 -0600

    Fixed movement
    Movement now uses a function with GetAxis instead of AddForce.

--- Assets/Scripts/PlayerBasicMovement.cs
+++ Assets/Scripts/PlayerBasicMovement.cs
@@ -18,29 +18,24 @@ public class PlayerBasicMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddForce(Vector3.left * movementSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.AddForce(Vector3.back * movementSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddForce(Vector3.right * movementSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            rb.AddForce(Vector3.forward * movementSpeed * Time.deltaTime);
-        }
+        PlayerMovement(movementSpeed);
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W))
         {
             animate.SetBool("Walking", true);
         }
-        else
-        {
-            animate.SetBool("Walking", false);
+        else 
+        { 
+            animate.SetBool("Walking", false); 
         }
+    }
+    void PlayerMovement(float moveSpeed)
+    {
+        //Grab the current horizontl and vertical positions of the player.
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        //we will gather the direction and speed based on the variables multiplied by time instead of frames.
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * moveSpeed * Time.deltaTime;
+        //This setting moves our player based on the value of playerMovement, using the RIGIDBODY attached to the player.
+        transform.Translate(playerMovement, Space.Self);
     }
 }
