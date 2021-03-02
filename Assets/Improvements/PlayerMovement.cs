4476143581f03b1dc45ec99652f879cf65823149
Paul <sevath1996@gmail.com>
2021-03-02 00:12:33 -0600

    Added Root Motion Run or walk option

--- Assets/Improvements/PlayerMovement.cs
+++ Assets/Improvements/PlayerMovement.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField, Tooltip("The max speed of the player")]
-    private float speed = 4f;
+    private float speed, runSpeed, currSpeed;
 
     private Animator animator;
 
@@ -17,8 +17,15 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
         input = Vector3.ClampMagnitude(input, 1f);
-        input *= speed;
+        input *= currSpeed;
         animator.SetFloat("Horizontal", input.x);
         animator.SetFloat("Vertical", input.z);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currSpeed = runSpeed;
+        } else
+        {
+            currSpeed = speed;
+        }
     }
 }
