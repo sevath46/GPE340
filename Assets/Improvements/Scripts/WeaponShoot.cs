10231bb294947ccebf978b4cb2052033000418f7
Paul <sevath1996@gmail.com>
2021-03-06 13:47:20 -0600

    Moved assets around a bit

--- Assets/Improvements/Scripts/WeaponShoot.cs
+++ Assets/Improvements/Scripts/WeaponShoot.cs
@@ -2,8 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponShoot : MonoBehaviour
+public class WeaponShoot : Weapon
 {
-    public GameObject barrel, bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) 
+        {
+            animate.SetTrigger("fire");
+            Debug.Log("Fired");
+        }
+    }
 }
