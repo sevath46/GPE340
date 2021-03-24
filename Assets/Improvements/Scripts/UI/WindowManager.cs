c52cd8244f5e7097334e62fc1662bf328ca99316
Paul <sevath1996@gmail.com>
2021-03-24 08:33:09 -0500

    Can set resolution and window mode in game.

--- Assets/Improvements/Scripts/UI/WindowManager.cs
+++ Assets/Improvements/Scripts/UI/WindowManager.cs
@@ -15,18 +15,23 @@ public class WindowManager : MonoBehaviour
 
     void Awake()
     {
-        Resolution[] resolutions = Screen.resolutions; //all resolution
+        Resolution[] resolutions = Screen.resolutions; //grabs all resolution
+        //for each resolution
         foreach (Resolution res in resolutions)
         {
-         resolutionsList.Add(res); //add resolution in list
-            resString.Add(res.width.ToString() + "x" + res.height.ToString()); //string format every resolution
+            //add resolution in list
+            resolutionsList.Add(res);
+            //add the resolution to a string list.
+            resString.Add(res.width.ToString() + "x" + res.height.ToString());
         }
+        //Add the entire list to the dropdown.
         resolutionDropdown.AddOptions(resString);
     }
 
 
     public void ScreenAdjust() 
     {
-        
+        //Set resolution of the screen and window vs fullscreen base on dropdown value.
+        Screen.SetResolution(resolutionsList[resolutionDropdown.value].width, resolutionsList[resolutionDropdown.value].height, fullScreen.isOn);
     }
 }
